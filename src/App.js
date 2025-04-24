c18f43cf7e3b2033c7e0f9e655e8e583a5e3ee34
GeekJulia <juliayaks148@gmail.com>
2025-04-24 11:20:18 +0100

    Trying to render component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import CryptoList from "./pages/CryptoList";
 const App =()=>{
     const [coinsData,setCoinsData] = useState([]);
 
-    useEffect(async () => {
-        const response = await axios.get(
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-        );
-        setCoinsData(response.data)
+    useEffect( () => {
+        const fetchData = async () => {
+            try{
+                const response = await axios.get(
+                    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+                );
+                setCoinsData(response.data)
+            }catch(error){
+                console.error("Error fetching data:",error)
+            }
+        }
     },[])
 
+    fetchData();
     return(
         <div className="app">
             <h1>Crypto Gallery</h1>
